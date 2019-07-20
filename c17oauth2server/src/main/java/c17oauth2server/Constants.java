35907fb1e4dbfd82ec18a96ec6e846f11c0bf97d
xMustang <xMustang@foxmail.com>
2019-07-20 13:44:48 +0800

    oauth2

---
+++ c17oauth2server/src/main/java/c17oauth2server/Constants.java
@@ -0,0 +1,12 @@
+package c17oauth2server;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-2-17
+ * <p>Version: 1.0
+ */
+public class Constants {
+
+    public static String RESOURCE_SERVER_NAME = "c17oauth2server-server";
+    public static final String INVALID_CLIENT_DESCRIPTION = "客户端验证失败，如错误的client_id/client_secret。";
+}
